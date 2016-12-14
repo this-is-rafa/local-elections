398e043bec023c663c2bedd1abcf23acc789b958
this-is-rafa <rafa@ratradio.net>
2016-12-14 18:21:45 -0500

    clean up for github

---
+++ README.md
@@ -0,0 +1,13 @@
+Miami-Dade Elections Parser
+
+See it in action here: http://local-elections.herokuapp.com
+
+I'm interested in local elections. Miami-Dade County provides great data on election nights, but their website leaves a lot to be desired, and is a chore to use on mobile.
+
+On election night 2016 I quickly wrote this to parse and present the election data from Miami-Dade County in a mobile-friendly format for me to refresh on the go.
+
+Will it work for the next election? Probably, as they've been using the same format for the past two elections.
+
+Please note that the results for state and national contests only reflect votes cast in Miami-Dade County.
+
+
--- elections.rb
+++ elections.rb
@@ -3,8 +3,10 @@ require 'json'
 require 'nokogiri'
 require 'sinatra'
 
-
 get "/" do
+
+#Because they do some strange URL changes every time they update the results, we grab this base URL and see where it redirects us. 
+#For the next election, I assume this 'miami_split' URL will need to be updated, specifically the number at the end.
 
 miami_split = "http://results.enr.clarityelections.com/FL/Dade/64620/"
 split_response = HTTParty.get(miami_split)
@@ -12,63 +14,17 @@ split_dom = Nokogiri::HTML(split_response.body)
 redirect = split_dom.css("script")[0]['src'].split("/")[1]
 
 
-miami_json = "http://results.enr.clarityelections.com/FL/Dade/64620/#{redirect}/json/sum.json"      
+miami_json = "#{miami_split}#{redirect}/json/sum.json"      
 miami_response = HTTParty.get(miami_json)
 election_json = JSON.parse(miami_response.body)
 
-miami_url = "http://results.enr.clarityelections.com/FL/Dade/64620/#{redirect}/en/summary.html"
+miami_url = "#{miami_split}#{redirect}/en/summary.html"
 miami_url_response = HTTParty.get(miami_url)
 miami_dom = Nokogiri::HTML(miami_url_response.body)
 
 @last_update = miami_dom.css("span#wlu").inner_html.to_s.split(">").last
 
-
-fl_url = "http://enight.elections.myflorida.com/Constitutional/Amendment.aspx"
-fl_response = HTTParty.get(fl_url)
-fl_dom = Nokogiri::HTML(fl_response.body)
-
-flsolar_yes = fl_dom.css(".progressbar")[0].content
-flsolar_no = fl_dom.css(".progressbar")[1].content
-flweed_yes = fl_dom.css(".progressbar")[2].content
-flweed_no = fl_dom.css(".progressbar")[3].content
-
-#############################
-
-#key biscayne 51
-kbcouncil = election_json['Contests'][51]
-@kbcontest = kbcouncil["C"].capitalize #name of contest
-@kbnames = kbcouncil["CH"] #candidates 0..5
-@kbpercent = kbcouncil["PCT"] #percentage 0..5
-@kbvotes = kbcouncil["V"] #votes 0..5
-@kbtotal = kbcouncil["TP"]
-@kbreport = kbcouncil["PR"]
-
-#mayor 33
-mayor = election_json['Contests'][33]
-@mayorcontest = mayor["C"].capitalize #name of contest
-@mayornames = mayor["CH"] #candidates 0..1
-@mayorpercent = mayor["PCT"] #percentage
-@mayorvotes = mayor["V"] #votes
-@mayortotal = mayor["TP"]
-@mayorreport = mayor["PR"]
-
-#solar 69
-solar = election_json['Contests'][69]
-@solarcontest = solar["C"].capitalize #name of contest
-@solarnames = solar["CH"] #yes - no 0..1
-@solarpercent = []
-@solarpercent << flsolar_yes << flsolar_no
-
-#weed 70
-weed = election_json['Contests'][70]
-@weedcontest = weed["C"].capitalize #name of contest
-@weednames = weed["CH"] #yes - no 0..1
-@weedpercent = []
-@weedpercent << flweed_yes << flweed_no
-
-
+@contests = election_json['Contests']
 
   erb :index
 end
-
-#############################
